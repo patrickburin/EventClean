0aa37218427564c6c7be9c76507fff479ae0e3ba
PatrickBurinR <patrick.burin@imbot.com.br>
2026-05-13 15:14:55 -0300

    first commit

---
+++ src/main/java/dev/java10x/EventClean/EventCleanApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.EventClean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventCleanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/EventClean/EventCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.EventClean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
